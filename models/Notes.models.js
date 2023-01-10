7d7f5dadb53cb221e622a8685d1b7d0a4b499e64
YuvRaj6392 <74025229+YuvRaj6392@users.noreply.github.com>
2023-01-10 15:22:28 +0530

    added models and dbConfig

---
+++ models/Notes.models.js
@@ -0,0 +1,11 @@
+module.export=mongoose=>{
+    const Note=mongoose.model('note',mongoose.Schema({
+        user:{
+            type:mongoose.Types.ObjectId, ref:'User'
+        },
+        title:{type:String, required:true},
+        description:{type:String, required:true},
+        tag:{type:String, default:"General"}
+    }))
+    return Note;
+}
---
+++ models/Users.models.js
@@ -0,0 +1,10 @@
+module.export=mongoose=>{
+    const User=mongoose.model('user',mongoose.Schema({
+        name:{type:String,required:true,index:true},
+        email:{type:String,required:true,unique:true},
+        password:{type:String,required:true,minlength:8,maxlength:32}
+    },{
+        timestamps:true
+    }))
+    return User;
+}
