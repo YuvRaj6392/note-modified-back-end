4c9122d16d76de9b9006e4a880a7d6187ccd954f
YuvRaj6392 <74025229+YuvRaj6392@users.noreply.github.com>
2023-01-16 16:37:41 +0530

    added changes

--- controllers/Users.controllers.js
+++ controllers/Users.controllers.js
@@ -1,29 +1,48 @@
-const db=require('../models');
-const User=db.users;
+const db = require("../models");
+const bcrypt = require("bcrypt");
+const User = db.users;
 
 //controller for the signup
-exports.signup=(req,res)=>{
-    res.json({
-        message:'signup'
-    })
-}
-
-
-
+exports.signup = (req, res) => {
+  User.findOne({ email: req.body.email }, (err, data) => {
+    if (data === null) {
+      const password = req.body.password;
+      const salt = bcrypt.genSaltSync(10);
+      const hash = bcrypt.hashSync(password, salt);
+      User.create({
+        email:req.body.email,
+        name:req.body.name,
+        password:hash
+      }).then(user=>{
+        res.status(200).json({
+            success:true,
+            message:user
+        })
+      }).catch(err=>{
+        res.status(500).json({
+            success:false,
+            message:'Some error occurred'
+        })
+      })
+    } else {
+        res.status(500).json({
+            success:false,
+            message:'Account already exists'
+        })
+    }
+  });
+};
 
 //controller for login
-exports.login=(req,res)=>{
-    res.json({
-        message:'login'
-    })
-}
-
-
-
+exports.login = (req, res) => {
+  res.json({
+    message: "login",
+  });
+};
 
 //controller for logout
-exports.logout=(req,res)=>{
-    res.json({
-        message:'logout'
-    })
-}
+exports.logout = (req, res) => {
+  res.json({
+    message: "logout",
+  });
+};
--- models/Users.models.js
+++ models/Users.models.js
@@ -2,7 +2,7 @@ module.exports=mongoose=>{
     const User=mongoose.model('user',mongoose.Schema({
         name:{type:String,required:true,index:true},
         email:{type:String,required:true,unique:true},
-        password:{type:String,required:true,minlength:8,maxlength:32}
+        password:{type:String,required:true,minlength:8}
     },{
         timestamps:true
     }));
